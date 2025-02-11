2d807440d74a92dcc44d7dcc386ea85d11868173
SebbieMzingKe <sebbievilar2@gmail.com>
2025-02-11 09:47:38 +0300

    chore: add leetcode 9

--- leetcode/longest_palindromic_string.py
+++ leetcode/longest_palindromic_string.py
@@ -5,24 +5,24 @@ class Solution(object):
         :rtype: str
         """
         res = ""
-        resLen = 0
+        res_len = 0
 
         for i in range(len(s)):
             # Odd length palindrome
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
+                if (r - l + 1) > res_len:
                     res = s[l:r+1]
-                    resLen = r - l + 1
+                    res_len = r - l + 1
                 l -= 1
                 r += 1
 
             # Even length palindrome
             l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
+                if (r - l + 1) > res_len:
                     res = s[l:r+1]
-                    resLen = r - l + 1
+                    res_len = r - l + 1
                 l -= 1
                 r += 1
 
