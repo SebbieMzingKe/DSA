b7e0ff2aed469e5292335bd47122ce29d5f38d10
SebbieMzingKe <sebbievilar2@gmail.com>
2025-11-17 12:34:00 +0300

    feat: implement solution for daily streak problem 1437 -> check-if-all-1s-are-at-least-length-k-places-away.

---
+++ check-if-all-1s-are-at-least-length-k-places-away.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def kLengthApart(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        prev_index = -float('inf') #last position of 1
+
+        for i, num in enumerate(nums):
+            if num == 1:
+                if i - prev_index - 1 < k:
+                    return False
+        return True
